ed2f942580c8e1858660556460bec7875be46469
AnjaniPatel-developer <anjanypatel@gmail.com>
2026-09-05 09:57:56 +0530

    Fix CORS: remove duplicate require and clean code

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ const cors = require("cors")
 app.use(cors({
     origin: [
         'http://localhost:5173',
+        
         'https://apni-dukan-76jh.onrender.com'  
     ],
     optionsSuccessStatus: 200,
